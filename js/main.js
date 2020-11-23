04e49414e18e885050469e4b77dbedd016b680ae
sandcreeker <cheef.trifonov@ya.ru>
2020-11-23 15:14:56 +0300

    create: added slide-key in swiper

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@ var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
+
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+})
+
+//$(document).keydown(function(e){
+//  if (e.which == 37) {
+//    $('.slider-button--prev')[0].click();
+//  };
+//  if (e.which == 39) {
+ //   $('.slider-button--next')[0].click();
+ // };
+//});
+
+$(document).ready(function(){
+  $('.slider-button--next').keydown(function(){
 });
